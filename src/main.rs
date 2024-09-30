2aa417da3f2d94101d4e7e241f9c5f28a48dbf4b
Johannes Schade <jo.schade@posteo.de>
2024-09-30 18:57:05 +0200

    refactoring in two files, reference using, pub markings

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,5 @@
-#[derive(Debug, Clone)]
-struct Quarternion {
-    real: f64,
-    i: f64,
-    j: f64,
-    k: f64,
-}
-
-impl Quarternion {
-    fn add(&self, q: Quarternion) -> Quarternion {
-        Quarternion {
-            real: self.real + q.real,
-            i: self.i + q.i,
-            j: self.j + q.j,
-            k : self.k + q.k,
-        }
-    }
-
-    fn mult(&self, q: Quarternion) -> Quarternion {
-        Quarternion {
-            real: self.real * q.real - self.i * q.i - self.j * q.j -self.k * q.k,
-            i: self.real * q.i + self.i * q.real + self.j * q.k - self.k * q.j,
-            j: self.real * q.j + self.j * q.real + self.k * q.i - self.i * q.k,
-            k: self.real * q.k + self.k * q.real + self.i + q.j - self.j * q.i,
-    }
-    }
-
-    fn conj(&self) -> Quarternion {
-        Quarternion {
-            real: self.real,
-            i: - self.i,
-            j: - self.j,
-            k: - self.k,
-        }
-    }
-
-    fn dot(&self, q: Quarternion) -> f64 {
-        self.mult(q.conj()).real
-    }
-
-    fn abs(&self) -> f64 {
-        self.dot(self.clone())
-    }
-
-    fn id() -> Quarternion {
-        Quarternion {
-            real: 1.0,
-            i: 0.0,
-            j: 0.0,
-            k: 0.0,
-        }
-    }
-
-    fn zero() -> Quarternion {
-        Quarternion {
-            real: 0.0,
-            i: 0.0,
-            j: 0.0,
-            k: 0.0,
-        }
-    }
-}
+mod quarternion;
+use quarternion::Quarternion;
 
 fn main() {
     let q = Quarternion {
@@ -76,6 +16,11 @@ fn main() {
         k: 4.8,
     };
 
+    println!("{:?}", p.add(&q));
+    println!("{:?}", p.mult(&q));
 
-    println!("{:?}", p.abs())
+    println!("{:?}", p.abs());
+    println!("{:?}", p.conj());
+    println!("{:?}", Quarternion::id());
+    println!("{:?}", Quarternion::zero());
 }
