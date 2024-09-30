5e54b7981b6b76b960fc972c450a162f49bb5648
Johannes Schade <jo.schade@posteo.de>
2024-09-30 11:57:34 +0200

    struct created, method add impelented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+#[derive(Debug)]
+struct Quarternion {
+    real: f64,
+    i: f64,
+    j: f64,
+    k: f64,
+}
+
+impl Quarternion {
+    fn add(&self, q: Quarternion) -> Quarternion {
+        Quarternion {
+            real: self.real + q.real,
+            i: self.i + q.i,
+            j: self.j + q.j,
+            k : self.k + q.k,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let q = Quarternion {
+        real: 1.0,
+        i: 2.0,
+        j: 2.0,
+        k: 4.0,
+    };
+
+    let p = Quarternion {
+        real: 1.5,
+        i: 2.6,
+        j: 2.7,
+        k: 4.8,
+    };
+
+
+    println!("{:?}", q.add(p))
 }
